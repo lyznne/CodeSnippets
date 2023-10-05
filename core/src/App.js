0b8ab94f4baa31c52ec89f1d30889e39d032ef3a
enos lyznne <emuthiani26@gmail.com>
2023-10-05 20:46:54 +0300

    ✨  client

--- core/src/App.js
+++ core/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Footer />
+      <Header /> 
+      <Footer />     
     </div>
   );
 }
---
+++ core/src/components/Whatthisis/Whatthisis.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Whatthisis = () => {
+  return (
+    <div>Whatthisis</div>
+  )
+}
+
+export default Whatthisis;
